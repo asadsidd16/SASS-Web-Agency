3638b919cc3bb16676c86c1112d1853833844fc4
asadsidd16 <asadsidd22@outlook.com>
2025-03-26 12:14:52 -0700

    more added to features section

---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/feature-1.png",
+    caption: "caption",
+    title: "title",
+    text: "text",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "watch the demo",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/feature-2.png",
+    caption: "caption",
+    title: "title",
+    text: "text",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "watch the demo",
+    },
+  },
+];
